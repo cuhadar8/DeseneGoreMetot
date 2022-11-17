3a613e0efc9b2d611e76ba9958586af28e82b082
İbrahim <68855464+cuhadar8@users.noreply.github.com>
2022-11-17 22:21:57 +0300

    Update Main.java

--- DeseneGoreMetot/src/Main.java
+++ DeseneGoreMetot/src/Main.java
@@ -14,7 +14,10 @@ public class Main {
 	}
 	
 	public static void main(String[] args) {
-		createDesign(16);
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("N sayısı: ");
+		int n = scanner.nextInt();
+		createDesign(n);
 	}
 
 }
