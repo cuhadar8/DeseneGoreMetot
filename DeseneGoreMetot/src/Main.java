39cf976452b422253aa552c25e30c5e34a381621
cuhadar8 <ibrahimcuhadar8@gmail.com>
2022-11-17 22:05:55 +0300

    DeseneGoreMetot

---
+++ DeseneGoreMetot/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void createDesign(int number) {
+		
+		System.out.print(number + " ");  //16 11 6 1 -4 çıktısını burası bastırıyor
+		
+		if(number > 0) {
+			createDesign(number-5);
+			System.out.print(number + " ");  //daha sonra if bloğuna giren en son sayı olan 1 den devam ederek geri dönüyor ve
+											//1 6 11 16 burada bastırılıyor
+		}
+	}
+	
+	public static void main(String[] args) {
+		createDesign(16);
+	}
+
+}
